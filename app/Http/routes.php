07e54c76dae5264da168a22535439a54d4c072b2
smlparry <samuel.parry@bigpond.com>
2015-06-04 23:56:37 +1000

    name and format routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,10 +1,27 @@
 <?php
 
+// Home route to show all tasks
 $app->get('/', function() {
   return view('list')->with('items', \App\Models\Item::all());
 });
-$app->post('/list/add', ['as' => 'add_list_item', 'uses' => 'App\Http\Controllers\ListController@add']);
-$app->post('/list/check', 'App\Http\Controllers\ListController@check');
-$app->post('/list/uncheck', 'App\Http\Controllers\ListController@unCheck');
-$app->post('/list/remove', 'App\Http\Controllers\ListController@remove');
+
+// Add a task
+$app->post('/list/add',
+           ['as' => 'add_list_item',
+           'uses' => 'App\Http\Controllers\ListController@add']);
+
+// Check a task as complete
+$app->post('/list/check',
+          ['as' => 'check_list_item',
+           'uses' => 'App\Http\Controllers\ListController@check']);
+
+// Uncheck a task
+$app->post('/list/uncheck',
+          ['as' => 'uncheck_list_item',
+           'uses' => 'App\Http\Controllers\ListController@unCheck']);
+
+// Remove a task
+$app->post('/list/remove',
+          ['as' => 'remove_list_item',
+           'uses' => 'App\Http\Controllers\ListController@remove']);
 
